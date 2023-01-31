ffc066df1147f9174ebd0a02c1103ee995200a7b
BondarenkoTet <111590245+BondarenkoTet@users.noreply.github.com>
2023-01-31 07:39:31 +0200

    HW-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const imgContainer = document.querySelector(".gallery");
+const imgMarkup = createGalleryImgMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+
+function createGalleryImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}"
+                alt="${description}" />
+        </a>
+    `;
+    })
+    .join("");
+}
+
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    scrollZoom: false,
+  });
 console.log(galleryItems);
